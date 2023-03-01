07595afdc33891b0a48b175b4f379bca79d3ebdd
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2023-03-01 15:22:26 -0500

    migrate to new manifest format

--- lib/bridgetown-docs-template.rb
+++ lib/bridgetown-docs-template.rb
@@ -2,8 +2,10 @@
 
 require "bridgetown"
 
-Bridgetown::PluginManager.new_source_manifest(
-  origin: BridgetownDocsTemplate,
-  components: File.expand_path("../components", __dir__),
-  layouts: File.expand_path("../layouts", __dir__)
-)
+Bridgetown.initializer :"bridgetown-docs-template" do |config|
+  config.source_manifest(
+    origin: BridgetownDocsTemplate,
+    components: File.expand_path("../components", __dir__),
+    layouts: File.expand_path("../layouts", __dir__)
+  )
+end
